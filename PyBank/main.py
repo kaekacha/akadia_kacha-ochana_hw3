ba7157a255c54dfb7391565f0aa6f7fc6ee79122
kaekacha <56618353+kaekacha@users.noreply.github.com>
2021-01-14 22:11:36 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,34 +3,32 @@ import csv
 
 csvpath = os.path.join('Resources', 'PyBank_Resources_budget_data.csv')
 with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    #print(csvreader)
-    csv_header = next(csvreader)
-    #print(f"CSV Header: {csv_header}")
+    csvreader = csv.reader(csvfile, delimiter=',') #to print data, could run print(csvreader)
+    csv_header = next(csvreader) #to print data, could run print(f"CSV Header: {csv_header}")
 
-    datelist=[]
+    datelist=[] #this variable and the two after create placeholder/empty lists for the variables
     profloss=[]
     change=[]
-    for row in csvreader:
-        datelist.append(row[0])
-        profloss.append(int(row[1]))
-    #print(datelist)
-    #print(profloss)
-    #print(len(datelist))
-    #print(sum(profloss))
+    for row in csvreader: 
+        datelist.append(row[0]) #in this for loop, the dates list is appended with the data in column A (index 0) 
+        profloss.append(int(row[1])) #within the same for loop, the profloss list is appended with the data in column B (index 1)
 
-    for x in range(1,len(profloss)): #or range(0,len(net)-1)
-        change.append(profloss[x]-profloss[x-1]) 
-        avchange="{:.2f}".format((sum(change)/len(change)))#sum of hcnages divided by total number of changes
+    for x in range(1,len(profloss)): #or range(0,len(profloss)-1)
+        change.append(profloss[x]-profloss[x-1]) #or change.append(profless[x+1]-profless[x])
+        avchange="{:.2f}".format((sum(change)/len(change))) #sum of changes divided by total number of changes, adding format for 2 decimal places
 
+    #finding max of the change (which is a list decreased by one to length 85),
+        #Use the index+1 to pull the max month from the date list (to accomodate date list length of 86)
     maxprof_index=change.index(max(change))
     maxprof=max(change)
     maxprof_month=datelist[maxprof_index+1]
    
+    #finding min of the change similar to above
     minprof_index=change.index(min(change))
     minprof=min(change)
     minprof_month=datelist[minprof_index+1]
 
+    #print in terminal the findings
     print("Financial Analysis")
     print("--------------------------")
     print(f'Total Months: {len(datelist)}')
@@ -39,6 +37,7 @@ with open(csvpath) as csvfile:
     print(f'Greatest Increase in Profits: {maxprof_month} (${maxprof})')
     print(f'Greatest Decrease in Profits: {minprof_month} (${minprof})')
 
+    #create a variable to print findingsto a txt file
     mystring=f"""
     Financial Analysis
     ----------------------------------
@@ -49,7 +48,7 @@ with open(csvpath) as csvfile:
     Greatest Decrease in Profits: {minprof_month} (${minprof})
     """
 
-    # Specify the file to write to
+    #Printing txt file to Analysis folder
     txtoutput = open("Analysis/pybankoutput.txt", "w")
     txtoutput.write(mystring)
     output_path = os.path.join("Analysis", "pybankoutput.txt")
